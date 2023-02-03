24122aa21e84ffb3a05c1da307c599ea36b7d78f
Hareon2 <king.leon.u@gmail.com>
2023-02-03 17:58:19 +0300

    Initital commit

--- js/script.js
+++ js/script.js
@@ -1,41 +1,9 @@
-function ibg(){
-    let ibg=document.querySelectorAll(".ibg");
-    for (var i = 0; i < ibg.length; i++) {
-        if(ibg[i].querySelector('img')){
-            ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
-        }
-    }
-}
 
-ibg();
-
-
-const iconMenu = document.querySelector('.header-menu__icon');
-if(iconMenu){
-    const menuBody = document.querySelector('.header-menu');
-    iconMenu.addEventListener("click",function (e){
-        menuBody.classList.toggle('active')
-        iconMenu.classList.toggle('active');
-       document.body.classList.toggle('lock')
-    });
-}
-
-let map;
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
-}
-
-window.initMap = initMap;
+$(window).scroll(function(event){
+       var s=0-$(this).scrollTop()/3;
+       $('.mainblock__image').css('transform','translate3d(0, '+s+'px, 0)');
+});
 
 
 
-
-
-
-
-
-
+     
